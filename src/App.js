dac2b1491c8bc0faaff3ed16a3d40ad25db41125
Faiq Allam <faiq.kaboel@gmail.com>
2020-03-26 17:40:07 +0700

    add redux dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { reduxForm, Field } from 'redux-form';
+// import logo from './logo.svg';
 import './App.css';
+
+let signInForm = props => {
+  const { handleSubmit } = props;
+  return <form onSubmit={handleSubmit} className="form">
+    <div className="field">
+      <div className="control">
+        <Field name="firstName" component={renderField} type="text" label="First Name"/>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <Field name="lastName" component={renderField} type="text" label="Last Name"/>
+      </div>
+    </div>
+  </form>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
