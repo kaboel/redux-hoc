77c495e25f008fe7adc4054870315b24d365adac
Faiq Allam <faiq.kaboel@gmail.com>
2020-03-26 18:07:59 +0700

    setup renderField comp

--- src/App.js
+++ src/App.js
@@ -112,6 +112,16 @@ const validate = val => {
   return errors;
  };
 
+ const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+  <div>
+    <div className="control">
+      <label className="field">{label}</label>
+      <input className="input" {...input} placeholder={label} type={type}/>
+      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+ )
+
 function App() {
   return (
     <div className="App">
