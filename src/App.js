00a6c3716f18187e39f71155a64a9f7056182488
Faiq Allam <faiq.kaboel@gmail.com>
2020-03-26 17:53:12 +0700

    setup form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 let signInForm = props => {
   const { handleSubmit } = props;
-  return <form onSubmit={handleSubmit} className="form">
+  return <form onSubmit={ handleSubmit } className="form">
     <div className="field">
       <div className="control">
         <Field name="firstName" component={renderField} type="text" label="First Name"/>
@@ -17,6 +17,71 @@ let signInForm = props => {
         <Field name="lastName" component={renderField} type="text" label="Last Name"/>
       </div>
     </div>
+
+    <div className="field">
+      <div className="control">
+        <Field name="email" component={renderField} type="text" label="Email"></Field>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <Field name="age" component={renderField} type="text" label="Age"/>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <label className="label">Proficiency</label>
+        <div className="select">
+          <Field className="input" name="proficiency" component="select">
+            <option/>
+            <option value="beginner">Beginner</option>
+            <option value="intermediate">Intermediate</option>
+            <option value="expert">Expert</option>
+          </Field>
+        </div>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <label className="label">Gender</label>
+        <label className="radio">
+          <Field name="gender" component="input" type="radio" value="male"/>
+          {''}
+          Male
+        </label>
+        <label className="radio">
+          <Field name="gender" component="input" type="radio" value="female"/>
+          {''}
+          Female
+        </label>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <label className="checkbox">
+          <Field name="saveDetails" id="saveDetails" component="input" type="checkbox"/>
+          Save Details
+        </label>
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <label className="label">Message</label>
+        <Field className="textarea" name="message" component="textarea" />
+      </div>
+    </div>
+
+    <div className="field">
+      <div className="control">
+        <button className="button is-link">Submit</button>
+      </div>
+    </div>
+
   </form>
 }
 
