627561f1440527bd5c8598c8b617bb5c79d8ddd1
Faiq Allam <faiq.kaboel@gmail.com>
2020-03-26 18:26:30 +0700

    style adjustments

--- src/App.js
+++ src/App.js
@@ -117,7 +117,7 @@ const renderField = ({ input, label, type, meta: { touched, error, warning } }) 
     <div className="control">
       <label className="field">{label}</label>
       <input className="input" {...input} placeholder={label} type={type}/>
-      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+      {touched && ((error && <span className="has-text-danger">{error}</span>) || (warning && <span>{warning}</span>))}
     </div>
   </div>
 )
