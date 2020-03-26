249c56aabc09c04b916269621e8ac01d3ad86574
Faiq Allam <faiq.kaboel@gmail.com>
2020-03-26 18:07:08 +0700

    setup validator

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { reduxForm, Field } from 'redux-form';
 // import logo from './logo.svg';
 import './App.css';
 
-let signInForm = props => {
+let SignInForm = props => {
   const { handleSubmit } = props;
   return <form onSubmit={ handleSubmit } className="form">
     <div className="field">
@@ -84,6 +84,33 @@ let signInForm = props => {
 
   </form>
 }
+
+const validate = val => {
+  const errors = {};
+  if (!val.firstName) {
+    console.log('First Name is required');
+    errors.firstName = 'Required';
+  }
+  if (!val.lastName) {
+    console.log('Last Name is required');
+    errors.lastName = 'Required';
+  }
+  if (!val.email) {
+    console.log('email is required');
+    errors.email = 'Required';
+  } else if (!/^.+@.+$/i.test(val.email)) {
+    console.log('email is invalid');
+    errors.email = 'Invalid email address';
+  }
+  if (!val.age) {
+    errors.age = 'Required'
+  } else if (isNaN(Number(val.age))) {
+    errors.age = 'Must be a number'
+  } else if (Number(val.age) < 18) {
+    errors.age = 'Sorry, you must be at least 18 years old'
+  }
+  return errors;
+ };
 
 function App() {
   return (
